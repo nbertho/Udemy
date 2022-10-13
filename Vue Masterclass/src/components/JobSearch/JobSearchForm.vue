d64cf3766761fe10dfb9a9718143d0966dc392be
Nicolas Bertho <nicolasbertho1996@gmail.com>
2022-10-13 10:03:37 +0200

    VM - Made search job form functional

--- Vue Masterclass/src/components/JobSearch/JobSearchForm.vue
+++ Vue Masterclass/src/components/JobSearch/JobSearchForm.vue
@@ -1,6 +1,7 @@
 <template>
   <form
     class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+    @submit.prevent="handleJobSearchForm"
   >
     <FontAwesomeIcon :icon="['fas', 'search']" class="ml-4 mr-3" />
 
@@ -41,6 +42,15 @@ export default {
     },
     updateLocation(payload) {
       this.location = payload;
+    },
+    handleJobSearchForm() {
+      this.$router.push({
+        name: "job-results",
+        query: {
+          role: this.role,
+          location: this.location,
+        },
+      });
     },
   },
 };
