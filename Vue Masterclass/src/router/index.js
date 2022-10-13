586409b23a94b56f4356eb3a98935c9cd2fa7b1e
Nicolas Bertho <nicolasbertho1996@gmail.com>
2022-10-13 09:53:56 +0200

    VM - Created  JobView view and lazy load view

--- Vue Masterclass/src/router/index.js
+++ Vue Masterclass/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "@/views/HomeView";
-import JobResultsView from "@/views/JobResultsView";
+
+// Lazy load page view
+const HomeView = () => import("@/views/HomeView");
+
+const JobResultsView = () =>
+  import(/* webpackChunkName: "jobs" */ "@/views/JobResultsView");
+
+const JobView = () => import(/* webpackChunkName: "jobs" */ "@/views/JobView");
 
 const routes = [
   {
@@ -12,6 +18,11 @@ const routes = [
     path: "/jobs/results",
     name: "job-results",
     component: JobResultsView,
+  },
+  {
+    path: "/jobs/results/:id",
+    name: "job-results-view",
+    component: JobView,
   },
 ];
 
---
+++ Vue Masterclass/src/views/JobView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Job page for job {{ currentJobId }}</h1>
+</template>
+
+<script>
+export default {
+  name: "JobView",
+  computed: {
+    currentJobId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
