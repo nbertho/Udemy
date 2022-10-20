f81b1f071cc33807f3b47507c70788d09e942924
Nicolas Bertho <nicolasbertho1996@gmail.com>
2022-10-20 14:14:57 +0200

    VM - Created Spotlight cards

---
+++ Vue Masterclass/src/components/JobSearch/Spotlight.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot :spotlight="spotlight"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+import constants from "@/data/constants";
+
+export default {
+  name: "Spotlight",
+  data() {
+    return {
+      spotlights: [],
+    };
+  },
+  async mounted() {
+    const response = await axios.get(`${constants.api.url}/spotlights`);
+    this.spotlights = response.data;
+  },
+};
+</script>
